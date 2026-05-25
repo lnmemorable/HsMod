b9fa49afa5fe3af401dec94a83e25c4302ccff54
lnmemorable <59554564+lnmemorable@users.noreply.github.com>
2026-05-25 13:13:38 +0800

    Update Main.cs

--- HsMod/Main.cs
+++ HsMod/Main.cs
@@ -208,7 +208,7 @@ namespace HsMod
                 }
                 else if (keyTimeGearMax.Value.IsDown())
                 {
-                    timeGear.Value = timeGear.Value >= 16 ? 64 : 16;
+                    timeGear.Value = timeGear.Value >= 24 ? 64 : 24;
                     return;
                 }
                 else if (keyTimeGearDefault.Value.IsDown())
